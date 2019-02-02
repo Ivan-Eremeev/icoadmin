21be08f474eeee432df2052bd0d9dbdb3feb45b7
Ivan Eremeev <ivan.frontcoder@gmail.com>
2019-02-02 22:41:47 +0500

    no message

--- js-app/libs-settings/slick_settings.js
+++ js-app/libs-settings/slick_settings.js
@@ -8,30 +8,31 @@
 // 	div
 // 	div
 
-$('.slider-for').slick({
-  slidesToShow: 1, // Сколько слайдов показывать на экране
+// $('.slider-for').slick({
+//   slidesToShow: 1, // Сколько слайдов показывать на экране
+//   slidesToScroll: 1, // Сколько слайдов пролистывать за раз
+//   dots: false, // Пагинация
+//   arrows: false, // Стрелки
+//   fade: true, // Плавный переход (анимация исчезновения появления) В false будет листаться
+//   asNavFor: '.slider-nav' // Связь со слайдерами
+// });
+
+var slider = $('.slider').slick({
+  slidesToShow: 3, // Сколько слайдов показывать на экране
   slidesToScroll: 1, // Сколько слайдов пролистывать за раз
+  // asNavFor: '.slider-for', // Связь со слайдерами
   dots: false, // Пагинация
   arrows: false, // Стрелки
-  fade: true, // Плавный переход (анимация исчезновения появления) В false будет листаться
-  asNavFor: '.slider-nav' // Связь со слайдерами
-});
-
-var slider = $('.slider-nav').slick({
-  slidesToShow: 1, // Сколько слайдов показывать на экране
-  slidesToScroll: 1, // Сколько слайдов пролистывать за раз
-  asNavFor: '.slider-for', // Связь со слайдерами
-  dots: true, // Пагинация
-  arrows: true, // Стрелки
   speed: 500, // Скорость перехода слайдов
   autoplay: false, // Автопрокрутка
   autoplaySpeed: 2000, // Скорость автопрокрутки
   centerMode: false, // Задает класс .slick-center слайду в центре
   focusOnSelect: true, // Выбрать слайд кликом
-  infinite: false, // Зацикленное пролистывание
+  infinite: true, // Зацикленное пролистывание
   vertical: false, // Вертикальный слайдер
   rtl: false, // Слайды листаются справа налево
-  centerPadding: '0px', // Отступы слева и справа чтоб увидеть часть крайних слайдов
+  centerMode: true, // Включить center mode, чтоб увидеть часть крайних слайдов
+  centerPadding: '212px', // Отступы слева и справа чтоб увидеть часть крайних слайдов
   adaptiveHeight: true, // Подгоняет высоту слайдера под элемент слайда
   variableWidth: false, // Подгоняет ширину слайдов под размер элемента,
   responsive: [	// Адаптация
